00fcdaeafdbd69ada099ca4c12f761c5fea01cdd
Jan-André Zinser <zinserjan@gmail.com>
2017-07-02 10:57:58 +0200

    fix snapshots (#15)
    closes #15

--- test/_util/stats.ts
+++ test/_util/stats.ts
@@ -5,7 +5,11 @@ import normalizePath = require("normalize-path");
 const normalizeError = (error: string) =>
   chalk
     .stripColor(error)
+    // replace all line endings
     .replace(/\r\n?/g, "\n")
+    // replace typescript messages, show only line:char + error
+    .replace(/(\(\d+,\d+\):\s\w*\sTS\d*)([\w\W]*)/, "$1")
+    // replace file path with xfile
     .replace(/([^('|"|\s|\w|\d)]|\w:)\.?((?:[^(\/\')]*\/)+)[^('||"\s)]*/gm, "xfile")
     .replace(/\.\.\.\./gm, "xfile");
 
