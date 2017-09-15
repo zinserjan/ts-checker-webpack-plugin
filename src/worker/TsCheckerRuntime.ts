6d981991ec317b83a3588d5bac2e78c8e86a7542
Jan-André Zinser <zinserjan@gmail.com>
2017-09-15 22:54:41 +0200

    pass config to child process simpler

--- src/worker/TsCheckerRuntime.ts
+++ src/worker/TsCheckerRuntime.ts
@@ -2,17 +2,21 @@ import * as path from "path";
 import IncrementalChecker from "../checker/IncrementalChecker";
 import { transformToWebpackBuildResult, serializeWebpackBuildResult } from "../checker/resultSerializer";
 
+export interface TsCheckerRuntimeConfig {
+  tsconfigPath: string;
+  diagnosticFormatter: string;
+  tslintPath?: string;
+  timings: boolean;
+}
+
 process.on("SIGINT", function() {
   process.exit(130);
 });
 
-const tsconfigPath = process.env.TSCONFIG;
-const diagnosticFormatter = process.env.DIAGNOSTIC_FORMATTER;
-const tslintPath = process.env.TSLINT;
-const timings = process.env.TIMINGS === "true";
-const contextPath = path.dirname(tsconfigPath);
+const config: TsCheckerRuntimeConfig = JSON.parse(process.env.TS_CHECKER_CONFIG);
+const contextPath = path.dirname(config.tsconfigPath);
 
-const incrementalChecker = new IncrementalChecker(timings, tsconfigPath, tslintPath);
+const incrementalChecker = new IncrementalChecker(config.timings, config.tsconfigPath, config.tslintPath);
 
 const messageOk = {
   id: "ok",
@@ -41,7 +45,7 @@ process.on("message", function(message: any) {
       incrementalChecker.updateBuiltFiles(message.files);
       const result = incrementalChecker.run();
 
-      const webpackResult = transformToWebpackBuildResult(result, contextPath, diagnosticFormatter);
+      const webpackResult = transformToWebpackBuildResult(result, contextPath, config.diagnosticFormatter);
       const serialized = serializeWebpackBuildResult(webpackResult);
 
       sendMessage({
