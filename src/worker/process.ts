82d6d1026d97fdd8ae5be1518f4b455780199408
Jan-André Zinser <zinserjan@gmail.com>
2017-09-20 09:57:46 +0200

    Fix types

--- src/worker/process.ts
+++ src/worker/process.ts
@@ -1,7 +1,11 @@
-import { fork, ForkOptions } from "child_process";
+import { fork, ForkOptions, ChildProcess } from "child_process";
 
 export const getProcess = () => process;
 
-export const forkProcess = (modulePath: string, args: string[], options?: ForkOptions) => {
+export const forkProcess: (modulePath: string, args: string[], options?: ForkOptions) => ChildProcess = (
+  modulePath: string,
+  args: string[],
+  options?: ForkOptions
+) => {
   return fork(modulePath, args, options);
 };
