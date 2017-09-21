b13e8a0d228cdc327a4a9430993a62ac920ef579
Jan-André Zinser <zinserjan@gmail.com>
2017-09-21 17:57:31 +0200

    test case: check class hierarchy types (#49)

---
+++ test/watchCases/class-hierarchy-types/steps/0/component-level-2.ts
@@ -0,0 +1,4 @@
+import { ComponentLevel1 } from "./component-level-1";
+
+
+export class ComponentLevel2 extends ComponentLevel1 {}
---
+++ test/watchCases/class-hierarchy-types/steps/2/entry.ts
@@ -0,0 +1,4 @@
+import { ComponentLevel4 } from "./component-level-4";
+
+const x = new ComponentLevel4();
+x.doStuff("5");
